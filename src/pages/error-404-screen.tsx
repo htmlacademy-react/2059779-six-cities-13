2de19a16e0259ee9eb65959433825d1ff02ba4cc
Vladimir Trofimov <i_am@palant.org>
2023-07-09 19:44:51 +0300

    Adds 404 page

---
+++ src/pages/error-404-screen.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
+function Error404Screen(): JSX.Element {
+	return (
+		<div className="page__favorites-container container">
+			<Helmet>
+				<title>Page Not Found</title>
+			</Helmet>
+			<section className="favorites favorites--empty">
+				<h1>Page Not Found</h1>
+				<div className="favorites__status-wrapper">
+					<p className="favorites__status-description">
+						<Link to="/">To main page</Link>
+					</p>
+				</div>
+			</section>
+		</div>
+	);
+}
+
+export { Error404Screen };
