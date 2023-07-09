f72235d38ef5b390c4bf91c8a6236423f347d73a
Vladimir Trofimov <i_am@palant.org>
2023-07-09 21:01:14 +0300

    Adds helmet provider

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { AppRoute } from '../../const';
 import MainPage from '../../pages/main-page';
 import Favorites from '../../pages/favorites';
@@ -12,30 +13,33 @@ type AppProps = {
 
 function App({ placesCount }: AppProps): JSX.Element {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path={AppRoute.Main}
-					element={<MainPage placesCount={placesCount} />}
-				/>
-				<Route
-					path={AppRoute.Favorites}
-					element={<Favorites />}
-				/>
-				<Route
-					path={AppRoute.Login}
-					element={<Login />}
-				/>
-				<Route
-					path={AppRoute.Offer}
-					element={<Offer />}
-				/>
-				<Route
-					path='*'
-					element={<Error404Screen />}
-				/>
-			</Routes>
-		</BrowserRouter>
+		<HelmetProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path={AppRoute.Main}
+						element={<MainPage placesCount={placesCount} />}
+					/>
+					<Route
+						path={AppRoute.Favorites}
+						element={<Favorites />}
+					/>
+					<Route
+						path={AppRoute.Login}
+						element={<Login />}
+					/>
+					<Route
+						path={AppRoute.Offer}
+						element={<Offer />}
+					/>
+					<Route
+						path='*'
+						element={<Error404Screen />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</HelmetProvider>
+
 	);
 }
 
