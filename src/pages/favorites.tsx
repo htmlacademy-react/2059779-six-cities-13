a5b59a7b8359e6f6d49db4092d6d8c6c418575d7
Vladimir Trofimov <i_am@palant.org>
2023-07-10 08:33:17 +0300

    Adds footer component

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
 
 function Favorites(): JSX.Element {
 	return (
@@ -180,17 +181,7 @@ function Favorites(): JSX.Element {
 					</section>
 				</div>
 			</main>
-			<footer className="footer container">
-				<a className="footer__logo-link" href="main.html">
-					<img
-						className="footer__logo"
-						src="img/logo.svg"
-						alt="6 cities logo"
-						width={64}
-						height={33}
-					/>
-				</a>
-			</footer>
+			<Footer />
 		</div>
 	);
 }
