6402246c6cb549f65a00665e9a2835debe753328
Vladimir Trofimov <i_am@palant.org>
2023-07-12 20:41:06 +0300

    Updates full offer template host pro status class

--- src/pages/offer-page.tsx
+++ src/pages/offer-page.tsx
@@ -91,7 +91,10 @@ function OfferPage({ fullOffer, reviews }: OfferPagePros): React.JSX.Element {
 							<div className="offer__host">
 								<h2 className="offer__host-title">Meet the host</h2>
 								<div className="offer__host-user user">
-									<div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
+									<div className={classNames(
+										'offer__avatar-wrapper', { 'offer__avatar-wrapper--pro': fullOffer.host.isPro }, 'user__avatar-wrapper'
+									)}
+									>
 										<img
 											className="offer__avatar user__avatar"
 											src={fullOffer.host.avatarUrl}
