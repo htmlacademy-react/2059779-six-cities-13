f494a080941c96c1143c5f12e682c4e8eaed6913
Vladimir Trofimov <i_am@palant.org>
2023-07-25 13:32:13 +0300

    Changes map tyle layer

--- src/hooks/use-map.ts
+++ src/hooks/use-map.ts
@@ -18,9 +18,9 @@ function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: Offer): leaf
 			});
 
 			const layer = new TileLayer(
-				'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+				'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 				{
-					attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+					attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 				},
 			);
 			instance.addLayer(layer);
