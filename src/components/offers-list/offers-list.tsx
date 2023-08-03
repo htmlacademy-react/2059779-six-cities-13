916a069fe13c45dc0f2829d99468b9915f39888e
Vladimir Trofimov <i_am@palant.org>
2023-08-03 16:12:28 +0300

    Adds sorting box component

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -1,4 +1,5 @@
 import OfferCard from '../offer-card/offer-card';
+import SortingBox from '../sorting-box/sorting-box';
 import LeafletMap from '../../components/leaflet-map/leaflet-map';
 import { Offer } from '../../mocks/offers';
 
@@ -17,33 +18,7 @@ function OffersList({ offersByCity, selectedCity, currentOffer, handleMouseEnter
 			<section className="cities__places places">
 				<h2 className="visually-hidden">Places</h2>
 				<b className="places__found">{offersByCity[selectedCity].length} places to stay in {selectedCity}</b>
-				<form className="places__sorting" action="#" method="get">
-					<span className="places__sorting-caption">Sort by</span>
-					{' '}
-					<span className="places__sorting-type" tabIndex={0}>
-						Popular
-						<svg className="places__sorting-arrow" width={7} height={4}>
-							<use xlinkHref="#icon-arrow-select" />
-						</svg>
-					</span>
-					<ul className="places__options places__options--custom places__options--opened">
-						<li
-							className="places__option places__option--active"
-							tabIndex={0}
-						>
-							Popular
-						</li>
-						<li className="places__option" tabIndex={0}>
-							Price: low to high
-						</li>
-						<li className="places__option" tabIndex={0}>
-							Price: high to low
-						</li>
-						<li className="places__option" tabIndex={0}>
-							Top rated first
-						</li>
-					</ul>
-				</form>
+				<SortingBox />
 				<div className="cities__places-list places__list tabs__content">
 					{offersByCity[selectedCity].map((offer) => <OfferCard item={offer} parentCSSClass='cities' onMouseEnter={() => handleMouseEnter(offer.id)} onMouseLeave={handleMouseLeave} key={offer.id} />)}
 				</div>
