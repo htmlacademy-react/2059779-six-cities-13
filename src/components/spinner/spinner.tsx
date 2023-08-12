26ac8c7f86d7280474fcbc6b41817340611c7fd3
Vladimir Trofimov <i_am@palant.org>
2023-08-12 14:25:21 +0300

    Adds spinner component

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,139 @@
+import styles from 'style.module.css';
+
+
+function Spinner(): React.JSX.Element {
+	return (
+		<div className={styles.content}>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+			<div className={styles.cuboid}>
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+				<div className={styles.side} />
+			</div>
+		</div>
+	);
+}
+
+export default Spinner;
